0c163fcb4916f850de60bccd371dbffac71690be
RickCZG <czg18366118377@yahoo.com>
2018-08-18 17:50:36 +0800

    修改测试文件

--- test/index.js
+++ test/index.js
@@ -2,13 +2,33 @@ const zexpress = require('../index');
 const app = zexpress();
 
 let port = 9876;
-app.get('/get', function (req, res) {
-  res.send('ZExpress ===> Get');
-})
+// app.get('/zexpress', function (req, res) {
+//   res.send('ZExpress ===> Get');
+// })
 
-app.post('/post', function (req, res) {
-  res.send('ZExpress ===> Post');
-})
+// app.post('/zexpress', function (req, res) {
+//   res.send('ZExpress ===> Post');
+// })
+
+// app.put('/zexpress', function (req, res) {
+//   res.send('ZExpress ===> Put');
+// })
+
+// app.delete('/zexpress', function (req, res) {
+//   res.send('Zexpress ===> Delete');
+// })
+
+app._router
+  .route('/user')
+  .get(function (req, res) {
+    res.send('user ===> Get');
+  })
+  .post(function (req, res) {
+    res.send("user ===> Post");
+  })
+  .put(function (req, res) {
+    res.send("user ===> Put");
+  })
 
 app.listen(port, function () {
   
